074f2062a3b7386804f8dfd46f4284a309526551
Serio <seriobraganca01@gmail.com>
2024-01-11 10:56:28 +0530

    commit sub

---
+++ gitproject/src/gitproject/subtract.java
@@ -0,0 +1,21 @@
+package gitproject;
+
+import java.util.Scanner;
+
+public class subtract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,sub;
+		System.out.println("enter the number");
+		Scanner Scan=new Scanner(System.in);
+		x=Scan.nextInt();
+		y=Scan.nextInt();
+		sub=x-y;
+		System.out.println("the sub is:"+sub);
+		
+
+
+	}
+
+}
