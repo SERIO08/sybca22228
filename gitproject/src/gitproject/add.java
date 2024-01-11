2c519a125b05317841780ab838f0ff2628f64bce
Serio <seriobraganca01@gmail.com>
2024-01-11 10:47:52 +0530

    commit pull

---
+++ gitproject/src/gitproject/add.java
@@ -0,0 +1,21 @@
+package gitproject;
+import java.util.Scanner;
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int x,y,sum;
+		System.out.println("enter the number");
+		Scanner Scan=new Scanner(System.in);
+		x=Scan.nextInt();
+		y=Scan.nextInt();
+		sum=x+y;
+		System.out.println("the sum is:"+sum);
+		
+
+	}
+	
+
+}
